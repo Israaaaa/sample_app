75befdb6ce89fab49b0e647e99eb7f3ba1b07231
Israaaaa <isra@el-isa.com>
2014-09-17 08:47:57 -0400

    commit message

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,22 +3,80 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
-  #Start of profile
+
+  #Start of index
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+
+  end #End of index
+ 
+  #Start of profile page
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
-  end #End of profile
- 
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end #End of profile page
+
   #Start of signup page
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
-  end #End of signup page
+  end #End of sigup page
 
   #Start of signup
   describe "signup" do
@@ -27,14 +85,24 @@ describe "User pages" do
 
     let(:submit) { "Create my account" }
 
+    #Start of invalid information inside signup
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-    end
 
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end #End of invalid information inside signup
+
+    #Start of valid information inside sign up
     describe "with valid information" do
       before do
+
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
@@ -53,11 +121,11 @@ describe "User pages" do
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
-    end
-
+    end #End of valid information inside signup
   end #End of signup
 
-    describe "edit" do
+  #Start of Edit
+  describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
       sign_in user
@@ -70,12 +138,14 @@ describe "User pages" do
       it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
 
+    #Start of invalid information inside Edit
     describe "with invalid information" do
       before { click_button "Save changes" }
 
       it { should have_content('error') }
-    end
-    
+    end #End of invalid informatoin inside edit
+
+    #Start of valid information inside edit
     describe "with valid information" do
       let(:new_name)  { "New Name" }
       let(:new_email) { "new@example.com" }
@@ -92,7 +162,6 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
-    end 
-
- end
-end
+    end #End of valid information inside edit
+  end #End of Edit
+end #End of ALL user pages
